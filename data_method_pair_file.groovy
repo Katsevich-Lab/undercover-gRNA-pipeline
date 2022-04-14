be97c399c7b931bccc8cb78a1fd866301a3c4e25
Tim <tbarry2@andrew.cmu.edu>
2022-04-14 11:05:15 -0400

    smaller example

--- data_method_pair_file.groovy
+++ data_method_pair_file.groovy
@@ -1,5 +1,4 @@
-// data_method_pairs = [papalexi_gene: ["seurat_de", "schraivogel_method", "seurat_de_mixscape"],
-//                      schraivogel_tap: ["seurat_de", "schraivogel_method"]]
+//data_method_pairs = [schraivogel_tap: ["schraivogel_method", "seurat_de"],
+//                     papalexi_gene: ["seurat_de"]]
 
-data_method_pairs = [schraivogel_tap: ["schraivogel_method", "seurat_de"],
-                     papalexi_gene: ["seurat_de"]]
+data_method_pairs = [schraivogel_tap: ["schraivogel_method"]]

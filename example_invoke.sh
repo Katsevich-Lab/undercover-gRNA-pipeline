47c2271914149eca2d41c072900aefd454803374
Tim <tbarry2@andrew.cmu.edu>
2022-04-14 20:43:18 -0400

    specify work dir

--- example_invoke.sh
+++ example_invoke.sh
@@ -1,5 +1,9 @@
 export NXF_OPTS="-Xms500M -Xmx3G" # limit NF to 2 GB of memory
+source ~/.research_config
+
+mkdir $LOCAL_PROJECT_DATA_DIR"work"
 
 nextflow main.nf --data_file $PWD"/data_file.R" \
  --data_method_pair_file $PWD"/data_method_pair_file.groovy" \
- --result_dir $PWD
+ --result_dir $PWD \
+ -w $LOCAL_PROJECT_DATA_DIR"work"

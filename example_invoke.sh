7a6b5e62f4e73369542c4ed4068a7a12b436d5e7
Tim <tbarry2@andrew.cmu.edu>
2022-04-15 14:10:59 -0400

    remove workdir set from example_invoke.sh

--- example_invoke.sh
+++ example_invoke.sh
@@ -1,11 +1,6 @@
 export NXF_OPTS="-Xms500M -Xmx3G" # limit NF to 2 GB of memory
 source ~/.research_config
 
-work_dir=$LOCAL_PROJECT_DATA_DIR"work"
-rm -rf $work_dir
-mkdir $work_dir
-
 nextflow main.nf --data_file $PWD"/data_file.R" \
  --data_method_pair_file $PWD"/data_method_pair_file.groovy" \
  --result_dir $PWD \
- -w $work_dir

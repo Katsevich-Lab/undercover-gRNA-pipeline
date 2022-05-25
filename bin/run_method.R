753ec9fbbe52b0787ff0745fd26686acebbd8994
Tim <tbarry2@andrew.cmu.edu>
2022-05-25 13:56:16 -0400

    generalize pipeline to allow for all other cells as control group

--- bin/run_method.R
+++ bin/run_method.R
@@ -3,7 +3,7 @@
 # Get CL args; set sceptre2 offsite dir
 args <- commandArgs(trailingOnly = TRUE)
 dataset_name <- args[1]
-undercover_ntc_name <- args[2]
+test_ntc_name <- args[2]
 method_name <- args[3]
 ram_req <- args[4]
 
@@ -16,16 +16,23 @@ response_odm <- load_dataset_modality(dataset_name)
 grna_dataset_name <- paste0(sub('/[^/]*$', '', dataset_name), "/grna")
 gRNA_odm <- load_dataset_modality(grna_dataset_name)
 
-# perform the label swap
-gRNA_feature_covariates <- gRNA_odm |> get_feature_covariates()
-tab_target_types <- sort(table(gRNA_feature_covariates$target_type), decreasing = TRUE)
-new_label <- (tab_target_types[names(tab_target_types) != "non-targeting"] |> names())[1]
-gRNA_feature_covariates[undercover_ntc_name, "target_type"] <- new_label
-gRNA_odm_swapped <- gRNA_odm |> mutate_feature_covariates(target_type = gRNA_feature_covariates$target_type)
+# check the control group of the method in question
+control_group <- get_control_group_for_method(method_name)
+
+# if the control group is "non_targeting_cells", perform the label swap
+if (control_group == "non_targeting_cells") {
+  gRNA_feature_covariates <- gRNA_odm |> get_feature_covariates()
+  tab_target_types <- sort(table(gRNA_feature_covariates$target_type), decreasing = TRUE)
+  new_label <- (tab_target_types[names(tab_target_types) != "non-targeting"] |> names())[1]
+  gRNA_feature_covariates[test_ntc_name, "target_type"] <- new_label
+  gRNA_odm_to_passs <- gRNA_odm |> mutate_feature_covariates(target_type = gRNA_feature_covariates$target_type)
+} else { # otherwise, keep the gRNA_odm as is
+  gRNA_odm_to_passs <- gRNA_odm
+}
 
 # obtain the (response, gRNA) pairs to analyze
 response_gRNA_group_pairs <- data.frame(response_id = get_feature_ids(response_odm),
-                                        gRNA_group = undercover_ntc_name)
+                                        gRNA_group = test_ntc_name)
 
 # verify that "method" is a function within the lowmoi package
 if (!exists(x = method_name, where = "package:lowmoi", mode = "function")) {
@@ -38,7 +45,7 @@ if (!all(c("response_odm", "gRNA_odm", "response_gRNA_group_pairs") %in% formal_
 }
 
 result_df <- do.call(what = method_name, args = list(response_odm = response_odm,
-                                                     gRNA_odm = gRNA_odm_swapped,
+                                                     gRNA_odm = gRNA_odm_to_passs,
                                                      response_gRNA_group_pairs = response_gRNA_group_pairs))
 
 if (!identical(sort(colnames(result_df)), c("gRNA_group", "p_value", "response_id"))) {
@@ -47,8 +54,8 @@ if (!identical(sort(colnames(result_df)), c("gRNA_group", "p_value", "response_i
 
 # add columns indicating the undercover gRNA, dataset name, and method name
 out <- result_df |>
-  dplyr::mutate(undercover_gRNA = gRNA_group, gRNA_group = NULL, dataset = dataset_name, method = method_name, ram_req = ram_req) |>
-  dplyr::mutate_at(.vars = c("response_id", "undercover_gRNA", "dataset", "method", "ram_req"), .funs = factor)
+  dplyr::mutate(test_ntc = gRNA_group, gRNA_group = NULL, dataset = dataset_name, method = method_name, ram_req = ram_req) |>
+  dplyr::mutate_at(.vars = c("response_id", "test_ntc", "dataset", "method", "ram_req"), .funs = factor)
 
 # save result
 saveRDS(object = out, file = "raw_result.rds")

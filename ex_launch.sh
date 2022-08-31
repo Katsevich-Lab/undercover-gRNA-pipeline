96241787b48b5287f56c8c6c428b81ad2c15cd8a
Tim <tbarry2@andrew.cmu.edu>
2022-08-31 16:39:43 -0400

    update example to include Fisher exact

--- ex_launch.sh
+++ ex_launch.sh
@@ -3,9 +3,9 @@ source ~/.research_config
 
 curr_time=$(date '+%m%d%H%M')
 nextflow run "main.nf" \
- --data_method_pair_file $LOCAL_CODE_DIR"/sceptre2-manuscript/param_files/sceptre_debug.groovy" \
+ --data_method_pair_file $LOCAL_CODE_DIR"/sceptre2-manuscript/param_files/data_method_pair_file.groovy" \
  --result_dir $LOCAL_SCEPTRE2_DATA_DIR"results/undercover_grna_analysis" \
- --result_file_name "sceptre_debug.rds" \
+ --result_file_name "fisher_exact_trial.rds" \
  --grna_modality "assignment" \
  --group_size "1" \
  --is_group_size_frac "false" \
@@ -13,5 +13,4 @@ nextflow run "main.nf" \
  --is_partition_count_frac "false" \
  --machine_name $MACHINE_NAME \
  --time $curr_time \
- --genes_to_subsample "5" \
  -profile standard # -resume
